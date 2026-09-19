71fb7c7932bc57222877d74b21103e23011d8964
Mallireddy Mukesh Reddy <257232529+Mukesh11207@users.noreply.github.com>
2026-09-19 23:37:34 +0530

    Factorial of a number

---
+++ 01_Basics/Basic_Maths/factorial.cpp
@@ -0,0 +1,13 @@
+int fact(int x){
+    if(x == 0){
+        return 1;
+    }
+    return x * fact(x-1);
+}
+
+class Solution {
+public:
+    int factorial(int n) {
+        return fact(n);
+    }
+};
